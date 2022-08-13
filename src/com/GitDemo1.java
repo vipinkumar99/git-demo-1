c5bf775c147424183c72a880008187545a9d5322
vipinkumar99 <vipinkumar999945@gmail.com>
2022-08-13 08:38:53 +0530

    skipping the stage area

--- src/com/GitDemo1.java
+++ src/com/GitDemo1.java
@@ -4,5 +4,6 @@ public class GitDemo1 {
 
 	public static void main(String argu[]) {
 		System.out.println("This is git demo-1");
+		System.out.println("This is git demo-1");
 	}
 }
--- src/com/GitDemo2.java
+++ src/com/GitDemo2.java
@@ -4,6 +4,7 @@ public class GitDemo2 {
 
 	public static void main(String[] args) {
 		System.out.println("This git demo - 2");
+		System.out.println("This git demo - 2");
 	}
 
 }
